538dbca0cafad2d94672fcdeadfc6f66a5503040
sofylopdev <sofylopdev@gmail.com>
2022-03-29 16:48:30 +0100

    codelab1 kotlin

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{rollDice()}
+
+        val countUpButton: Button = findViewById(R.id.count_up)
+        countUpButton.setOnClickListener{countUp()}
+    }
+
+    private fun rollDice(){
+        val randomInt = (1..6).random()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()//"Dice Rolled!"
+     //   Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        val currentText: String =resultText.text.toString()
+        if(currentText == "Hello World!"){
+            resultText.text = 1.toString()
+        }else if(currentText.toInt() != 6){
+            resultText.text = (resultText.text.toString().toInt() +1).toString()
+        }
+    }
+}
